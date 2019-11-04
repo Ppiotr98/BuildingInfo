1b049a3e0aec983a1ff1499423c7a5686fc80836
Vieja <mwiechcz@gmail.com>
2019-11-04 22:03:07 +0100

    add 4 basic classes

---
+++ src/main/java/pl/put/poznan/transformer/app/Budynek.java
@@ -0,0 +1,25 @@
+package pl.put.poznan.transformer.app;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Budynek extends Lokalizacja {
+    private Set<Poziom> poziomy = new HashSet<>();
+
+    public Budynek(int id) {
+        super(id);
+    }
+
+    public Budynek(int id, String name) {
+        super(id, name);
+    }
+
+    public void setPoziomy(Set<Poziom> poziomy) {
+        this.poziomy = poziomy;
+        /* TODO */
+    }
+
+    public Set<Poziom> getPoziomy() {
+        return poziomy;
+    }
+}
