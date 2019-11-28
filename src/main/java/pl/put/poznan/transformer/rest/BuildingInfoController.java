9d3b0497b2f0b853d3868739364e986df61606bc
DanikTL <danik4tl@gmail.com>
2019-11-28 12:02:13 +0100

    action post method

--- src/main/java/pl/put/poznan/transformer/rest/BuildingInfoController.java
+++ src/main/java/pl/put/poznan/transformer/rest/BuildingInfoController.java
@@ -8,35 +8,36 @@ import java.util.Arrays;
 
 
 @RestController
-@RequestMapping("/{text}")
+@RequestMapping("/{action}")
 public class BuildingInfoController {
 
     private static final Logger logger = LoggerFactory.getLogger(BuildingInfoController.class);
 
+    // nie potrzebujemy metody get ponieważ musimy wysłać coś. UNUSED
     @RequestMapping(method = RequestMethod.GET, produces = "application/json")
-    public String get(@PathVariable String text,
+    public String get(@PathVariable String action,
                               @RequestParam(value="transforms", defaultValue="upper,escape") String[] transforms) {
 
         // log the parameters
-        logger.debug(text);
+        logger.debug(action);
         logger.debug(Arrays.toString(transforms));
 
         // do the transformation, you should run your logic here, below just a silly example
         BuildingInfo transformer = new BuildingInfo(transforms);
-        return transformer.transform(text);
+        return transformer.transform(action);
     }
 
     @RequestMapping(method = RequestMethod.POST, produces = "application/json")
-    public String post(@PathVariable String text,
-                      @RequestBody String[] transforms) {
+    public String post(@PathVariable String action,
+                      @RequestBody String[] r_body) {
 
         // log the parameters
-        logger.debug(text);
-        logger.debug(Arrays.toString(transforms));
+        logger.debug(action);
+        logger.debug(Arrays.toString(r_body));
 
         // do the transformation, you should run your logic here, below just a silly example
-        BuildingInfo transformer = new BuildingInfo(transforms);
-        return transformer.transform(text);
+        BuildingInfo transformer = new BuildingInfo(r_body);
+        return transformer.transform(action);
     }
 
 
